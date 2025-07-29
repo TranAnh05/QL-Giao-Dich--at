3b29ea4b46cbf677c85d911152e83925b6fbc9d9
TranAnh05 <anh900472@gmail.com>
2025-07-29 14:26:38 +0700

    create TransactionDTO

---
+++ src/persistence/TransactionDTO.java
@@ -0,0 +1,14 @@
+package persistence;
+
+import java.time.LocalDate;
+
+public class TransactionDTO {
+    public String transactionId;
+    public LocalDate transactionDate;
+    public double unitPrice;
+    public double area;
+    public String transactionType;
+    public String landType;
+    public String houseType; 
+    public String address;
+}
--- src/persistence/TransactionListViewDAO.java
+++ src/persistence/TransactionListViewDAO.java
@@ -1,5 +1,32 @@
 package persistence;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.sql.Statement;
+
 public class TransactionListViewDAO {
-    
+    private Connection conn;
+
+    public TransactionListViewDAO() throws SQLException, ClassNotFoundException {
+        
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        String username = "root";
+        String password = "130405";
+        String url = "jdbc:mysql://localhost:3306/transaction?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+        conn = DriverManager.getConnection(url, username, password);
+    }
+
+    public List<TransactionDTO> getAll() {
+        List<TransactionDTO> list = new ArrayList<TransactionDTO>();
+        Statement stmt = null;
+		ResultSet rs = null;
+
+        
+
+        return list;
+    }
 }
